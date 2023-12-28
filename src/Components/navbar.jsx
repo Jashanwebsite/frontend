e80841c552118a20f9fea447c4234f964b049e13
Jashanwebsite <jashandeepsingh61124@gmail.com>
2023-12-28 19:03:30 +0530

    navbar updated

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -12,11 +12,8 @@ function Navbarcomponent() {
     const remove =() => {
         localStorage.removeItem("token")
         navigate("frontend/login")
-        var bars = document.getElementById("nav-action");
-        var nav = document.getElementById("nav");
-        bars.classList.toggle('active');
-        nav.classList.toggle('visible');
-        // console.log(bars.classList)
+        barClicked();
+
     }
     let location = useLocation();
     useEffect(() => {
@@ -27,7 +24,7 @@ function Navbarcomponent() {
         var nav = document.getElementById("nav");
         bars.classList.toggle('active');
         nav.classList.toggle('visible');
-        console.log(bars.classList)
+        // console.log(bars.classList)
     }
     return (
         <div>
@@ -37,11 +34,11 @@ function Navbarcomponent() {
 
             <nav id="nav">
                 <ul>
-                {!localStorage.getItem("token") && <li onClick={barClicked} class="shape-circle circle-one"><Link to="/frontend/Signup">signup</Link></li>}
+                {!localStorage.getItem("token") && <li onClick={barClicked} class="shape-circle circle-one"><Link to="/Signup">signup</Link></li>}
                     {localStorage.getItem("token") &&  <li onClick={remove} class="shape-circle circle-two"><a href="">logout</a></li>}
-                    {!localStorage.getItem("token") &&   <li onClick={barClicked}  class="shape-circle circle-two"><Link to={"/frontend/login"}>login</Link></li>}
-                    {localStorage.getItem("token") &&   <li onClick={barClicked}  class="shape-circle circle-two"><Link to={"/frontend/about"}>about</Link></li>}
-                      <li onClick={barClicked} class="shape-circle circle-three"><Link to={"/frontend/home"}>home</Link></li>
+                    {!localStorage.getItem("token") &&   <li onClick={barClicked}  class="shape-circle circle-two"><Link to={"/login"}>login</Link></li>}
+                    {localStorage.getItem("token") &&   <li onClick={barClicked}  class="shape-circle circle-two"><Link to={"/about"}>about</Link></li>}
+                      <li onClick={barClicked} class="shape-circle circle-three"><Link to={"/home"}>home</Link></li>
                     <li class="shape-circle circle-five"></li>
                 </ul>
             </nav>
